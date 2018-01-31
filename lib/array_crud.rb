42342ec410cc945577bc8ab8548151556af04f53
Edward Norton <edwardnorton@icloud.com>
2018-01-31 23:01:54 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push(5)
+  arr = [1, 2, 3, 4]
+  arr.push(5)
 end
 
 def add_element_to_start_of_array(array, element)
